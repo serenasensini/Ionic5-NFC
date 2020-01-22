ad60cb7bc2ded0a4357b8bfe360ffc76ce2a5b7c
Serena Sensini <serena.sensini@gmail.com>
2020-01-22 18:03:27 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ndef, NFC} from '@ionic-native/nfc/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private nfc: NFC, private ndef: Ndef) { }
 
+  leggiNFC() {
+    this.nfc.addNdefListener(() => {
+      console.log('successfully attached ndef listener');
+    }, (err) => {
+      console.log('error attaching ndef listener', err);
+    }).subscribe((event) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+    });
+  }
+
+
+  scriviNFC() {
+    this.nfc.addNdefListener(() => {
+      console.log('successfully attached ndef listener');
+      const message = this.ndef.textRecord('Hello world');
+      this.nfc.share([message]).then(
+          value => {
+
+          }
+      ).catch(
+          reason => {
+            console.log(reason);
+          }
+      );
+    }, (err) => {
+      console.log('error attaching ndef listener', err);
+    });
+
+  }
 }
